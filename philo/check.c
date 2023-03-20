9c26a28c11acce786e9b9ae5aac3326e2fe718b8
aoudija <amineoudija02@yahoo.com>
2023-03-20 10:56:50 +0000

    commit

--- philo/check.c
+++ philo/check.c
@@ -6,11 +6,21 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 09:06:01 by aoudija           #+#    #+#             */
-/*   Updated: 2023/03/20 09:16:13 by aoudija          ###   ########.fr       */
+/*   Updated: 2023/03/20 10:50:05 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.c"
+#include "philo.h"
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = -1;
+	while (s[++i])
+		;
+	return (i);
+}
 
 int	ft_isdigit(int c)
 {
@@ -24,11 +34,11 @@ int	good_to_go(char **s, int ac)
 	int	i;
 	int	j;
 
-	i = -1;
+	i = 0;
 	while (++i < ac)
 	{
 		j = -1;
-		while (s[i][++j])
+		while (++j < ft_strlen(s[i]))
 		{
 			if (!ft_isdigit(s[i][j]))
 				return (0);
--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: aoudija <aoudija@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 11:00:09 by aoudija           #+#    #+#             */
-/*   Updated: 2023/03/20 09:18:20 by aoudija          ###   ########.fr       */
+/*   Updated: 2023/03/20 10:55:13 by aoudija          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,23 +63,26 @@ void	philo_n(t_data data)
 	data.t0 = get_time_ms();
 	uusleepp(300);
 	norm1(data);
-	// usleep(500);
 	i = -1;
 	while (++i < data.nph)
 		pthread_mutex_destroy(&data.fork[i]);
 	free(th);
 }
 
-void	fill(t_data data, char **av)
+void	amin(t_data data, char **av, int ac)
 {
-	data.nph = ft_atoi(av[1]);
-	data.fork = malloc(sizeof(pthread_mutex_t) * data.nph);
-	data.mutex = malloc(sizeof(pthread_mutex_t));
-	data.t_ate = malloc(sizeof(long) * data.nph);
-	data.ate = malloc(sizeof(int) * data.nph);
-	data.t_die = ft_atoi(av[2]);
-	data.t_eat = ft_atoi(av[3]);
-	data.t_sleep = ft_atoi(av[4]);
+	if (ac == 6)
+	{
+		data.times_e = ft_atoi(av[5]);
+		memset(data.ate, 0, data.nph * sizeof(int));
+		philo_n(data);
+	}
+	else if (ac == 5)
+	{
+		data.times_e = 1;
+		memset(data.ate, -1, data.nph * sizeof(int));
+		philo_n(data);
+	}
 }
 
 int	main(int ac, char **av)
@@ -88,21 +91,17 @@ int	main(int ac, char **av)
 
 	if (ac == 5 || ac == 6)
 	{
-		if (!good_to_go(av, ac));
+		if (!good_to_go(av, ac))
 			return (0);
-		fill (data, av);
-		if (ac == 6)
-		{
-			data.times_e = ft_atoi(av[5]);
-			memset(data.ate, 0, data.nph * sizeof(int));
-			philo_n(data);
-		}
-		else if (ac == 5)
-		{
-			data.times_e = 1;
-			memset(data.ate, -1, data.nph * sizeof(int));
-			philo_n(data);
-		}
+		data.nph = ft_atoi(av[1]);
+		data.fork = malloc(sizeof(pthread_mutex_t) * data.nph);
+		data.mutex = malloc(sizeof(pthread_mutex_t));
+		data.t_ate = malloc(sizeof(long) * data.nph);
+		data.ate = malloc(sizeof(int) * data.nph);
+		data.t_die = ft_atoi(av[2]);
+		data.t_eat = ft_atoi(av[3]);
+		data.t_sleep = ft_atoi(av[4]);
+		amin(data, av, ac);
 		return (free(data.fork), free(data.ate)
 			, free(data.t_ate), free(data.mutex), 0);
 	}
